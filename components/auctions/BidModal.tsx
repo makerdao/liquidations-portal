b2a607d9600054c58794bc942885d1489f04f8ba
Phil Bain <bain.pmcg@gmail.com>
2021-03-29 17:11:57 -0600

    update bid modal style, add deposit button

--- components/auctions/BidModal.tsx
+++ components/auctions/BidModal.tsx
@@ -42,12 +42,23 @@ const BidModal = ({
             : { variant: 'dialog.desktop', animation: `${fadeIn} 350ms ease`, width: '450px' }
         }
       >
-        <Flex sx={{ flexDirection: 'column' }}>
-          <Close sx={{ alignSelf: 'flex-end' }} onClick={onDismiss} />
-          <Heading>Place a Bid</Heading>
+        <Flex sx={{ flexDirection: 'column', pb: 3 }}>
+          <Close
+            sx={{
+              alignSelf: 'flex-end',
+              height: 4,
+              width: 4,
+              p: 0,
+              position: 'relative',
+              top: '-4px',
+              left: '8px'
+            }}
+            onClick={onDismiss}
+          />
+          <Heading sx={{ fontWeight: 'bold' }}>Place a Bid</Heading>
           <LogoBanner name={name.toUpperCase()} icon={COLLATERAL_LOGOS[name]} />
           <Flex sx={{ justifyContent: 'space-between', alignItems: 'flex-end', my: 2 }}>
-            <Text sx={{ fontWeight: 'bold' }}>Collateral Available</Text>
+            <Text sx={{ fontSize: 3, fontWeight: 'semiBold' }}>Collateral Available</Text>
             <Heading variant="mediumHeading">
               {collateralAvailable} {name.toUpperCase()}
             </Heading>
@@ -57,13 +68,15 @@ const BidModal = ({
             <Text>DAI in the VAT</Text>
             <Flex sx={{ flexDirection: 'column', alignItems: 'flex-end' }}>
               <Text>{fromRad(vatBalance).toFormat(2)} DAI</Text>
-              <Text>Deposit</Text>
+              <Button variant="textual" sx={{ color: 'primary', fontSize: 2, p: 0 }}>
+                Deposit
+              </Button>
             </Flex>
           </Flex>
           <Flex sx={{ flexDirection: 'column', my: 3 }}>
             <Flex sx={{ justifyContent: 'space-between' }}>
-              <Text sx={{ fontWeight: 'bold', fontSize: 3 }}>Amount of Dai</Text>
-              <Text sx={{ fontSize: 3 }}>
+              <Text sx={{ fontWeight: 'semiBold', fontSize: 3 }}>Amount of Dai</Text>
+              <Text sx={{ fontSize: 3, color: 'textMuted' }}>
                 Wallet balance: {daiBalance && daiBalance.toBigNumber().toFormat(2)}
               </Text>
             </Flex>
@@ -71,19 +84,19 @@ const BidModal = ({
             <Flex sx={{ justifyContent: 'space-between', my: 2, px: 2 }}>
               <Flex sx={{ flexDirection: 'column' }}>
                 <Text sx={{ color: 'textSecondary' }}>Dust limit</Text>
-                <Text>{dustLimit} Dai</Text>
+                <Text sx={{ color: 'textMuted' }}>{dustLimit} Dai</Text>
               </Flex>
               <Flex sx={{ flexDirection: 'column' }}>
                 <Text sx={{ color: 'textSecondary' }}>Max bid</Text>
-                <Text>{maxBid} Dai</Text>
+                <Text sx={{ color: 'textMuted' }}>{maxBid} Dai</Text>
               </Flex>
             </Flex>
           </Flex>
           <Divider />
           <Flex sx={{ justifyContent: 'space-between', alignItems: 'center', my: 2 }}>
             <Flex sx={{ flexDirection: 'column' }}>
-              <Text sx={{ fontWeight: 'bold' }}>Amount of Collateral</Text>
-              <Text variant="caps" sx={{ fontWeight: 'body', fontSize: 5, color: 'text', pl: 2 }}>
+              <Text sx={{ fontSize: 3, fontWeight: 'semiBold' }}>Amount of Collateral</Text>
+              <Text variant="caps" sx={{ fontWeight: 'body', fontSize: 5, color: 'textMuted', pl: 2 }}>
                 0.00 {name}
               </Text>
             </Flex>
