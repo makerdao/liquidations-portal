da6eb8573aefd916c0863abdc90c97d9b5e647c8
Phil Bain <bain.pmcg@gmail.com>
2021-11-23 19:09:45 -0700

    fix decimal issue with non-standard decimals

--- pages/auctions/[auction-type].tsx
+++ pages/auctions/[auction-type].tsx
@@ -65,11 +65,8 @@ export default function Auctions(): JSX.Element | null {
     const txCreator = () =>
       maker.service('liquidation').exitGemFromAdapter(
         ilk,
-        //account for non-standard decimals, also subtract one wei to avoid any rounding issues
-        vatGemBalance
-          .times(Math.pow(10, decimals - 18))
-          .minus(Math.pow(10, -18))
-          .toFixed(18)
+        //account for non-standard decimals, also round down to avoid any rounding issues
+        vatGemBalance.toFixed(decimals, BigNumber.ROUND_DOWN)
       );
     await transactionsApi.getState().track(txCreator, `Exiting ${vatGemBalance.toFormat(4)} ${ilk}`, {
       pending: () => {
